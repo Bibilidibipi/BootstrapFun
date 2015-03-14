5742ec8d604058517461bce7d4cd36199ffa1e5b
Bibilidibipi <judy0wacker@gmail.com>
2015-03-14 12:16:14 -0700

    add navbar styling

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,31 @@
+class SessionsController < ApplicationController
+  def create
+    # signs a user in
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user.nil?
+      # no user with the given name!
+      flash.now[:errors] = ["Invalid credentials"]
+      render :new
+    else
+      # sign the user in
+      log_in!(@user)
+      redirect_to user_url
+    end
+  end
+
+  def destroy
+    # sign a user out
+    log_out!
+    redirect_to new_session_url
+  end
+
+  def new
+    # presents a login form
+    @user = User.new
+    render :new
+  end
+end
